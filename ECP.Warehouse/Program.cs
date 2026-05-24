9e9262f1cee65c0f36a15b63b5fa5e709a5c4559
binary <khimup.ads@gmail.com>
2026-05-24 22:41:40 +0700

    Update to V5

--- ECP.Warehouse/Program.cs
+++ ECP.Warehouse/Program.cs
@@ -9,12 +9,27 @@ app.MapGet("/health/live", () => Results.Ok(new { status = "Healthy" }));
 app.MapGet("/health/ready", () => Results.Ok(new { status = "Ready" }));
 app.MapGet("/status", () =>  Results.Ok("The service warehouse is running...."));    
 app.MapGet("/", () =>  Results.Json("The service warehouse is running....")); 
-app.MapGet("/env", () => 
-    Results.Json(new
+
+app.MapGet("/env", () =>
+{
+    var hostProperties = builder.Host.Properties
+        .Select(x => new
+        {
+            key = x.Key.ToString(),
+            value = x.Value
+        });
+
+    return Results.Json(new
     {
         env = builder.Environment.EnvironmentName,
         service = builder.Environment.ApplicationName,
-        root = builder.Environment.WebRootPath
-    })); 
+        root = builder.Environment.ContentRootPath,
+        hostProperties
+    });
+});
 
 app.Run();
+
+// docker build -t epc.warehouse:v4 -f ECP.Warehouse/Dockerfile .
+// docker tag epc.warehouse:v4 devkhim/epc.warehouse:v4  
+// docker push devkhim/epc.warehouse:v4 
