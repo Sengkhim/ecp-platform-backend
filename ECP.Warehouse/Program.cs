e324e12ace34b6ca154a05760ecb193537f7c0fa
binary <khimup.ads@gmail.com>
2026-06-06 15:20:21 +0700

    Update to v6

--- ECP.Warehouse/Program.cs
+++ ECP.Warehouse/Program.cs
@@ -9,6 +9,7 @@ app.MapGet("/health/live", () => Results.Ok(new { status = "Healthy" }));
 app.MapGet("/health/ready", () => Results.Ok(new { status = "Ready" }));
 app.MapGet("/status", () =>  Results.Ok("The service warehouse is running...."));    
 app.MapGet("/", () =>  Results.Json("The service warehouse is running....")); 
+app.MapGet("/new", () =>  Results.Json("new")); 
 
 app.MapGet("/env", () =>
 {
